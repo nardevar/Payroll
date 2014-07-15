06c82cb943d0a1a4528e4bdecc43a721ae397878
nardevar <nardevar@in.ibm.com>
2014-07-15 18:14:23 +0530

    added comment

--- src/com/hdfc/core/Bank.java
+++ src/com/hdfc/core/Bank.java
@@ -2,6 +2,8 @@ package com.hdfc.core;
 
 public class Bank{
 	
+	//added comment in eclipse
+	
 	public static void main(String s[]){
 		AccountService accountService=new AccountService(1000,"Laden","S",12000);
 		accountService.details();
